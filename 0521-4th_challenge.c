5fde55e0d8e080b91abdf2e99f81e543cc94f407
internashionalist <taylor.thames@atlaschool.com>
2024-05-21 19:36:11 +0000

    4.2

--- 0521-4th_challenge.c
+++ 0521-4th_challenge.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 
+/**
+ * main - convert Celsius to Fahrenheit
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	float C, F;
 
 	printf("Enter temperature in Celsius: ");
 	scanf("%f", &C);
-	{
-		F = (C * 9 / 5) + 32;
-	}
 
-	printf("%.2f Celsius = %.2f Fahrenheit\n", C, F);
+	F = (C * 9 / 5) + 32;
+
+	printf("%.2f degrees Celsius = %.2f degrees Fahrenheit\n", C, F);
 
 	return (0);
 }
